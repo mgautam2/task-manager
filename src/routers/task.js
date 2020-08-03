0f42a8cab52d633015c761a93891ce837c511782
mgautam2 <mgautam2@illinois.edu>
2020-08-02 17:49:09 -0700

    Done avatar uploads

--- src/routers/task.js
+++ src/routers/task.js
@@ -31,7 +31,6 @@ router.get('/tasks', auth , async (req, res) => {
     match.completed = req.query.completed === 'true'
     
   }
-  console.log(sort)
     
     try {
         await req.user.populate({
@@ -40,15 +39,12 @@ router.get('/tasks', auth , async (req, res) => {
           options : {
             limit : parseInt(req.query.limit),
             skip : parseInt(req.query.skip),
-            sort: {
-              completed : 1
-            }
+            sort
           }
         }).execPopulate()
         
         res.send(req.user.tasks)
     } catch (e) {
-        console.log(e)
         res.status(500).send()
     }
 })
@@ -58,6 +54,7 @@ router.get('/tasks/:id', auth , async (req, res) => {
 
     try {
         const task = await Task.findOne({_id, owner : req.user._id})
+        task.populate('owner').exec
 
         if (!task) {
             return res.status(404).send("Bhag Bhosdike!!")
@@ -95,7 +92,6 @@ router.patch('/tasks/:id', auth , async (req, res) => {
 router.delete('/tasks/:id', auth, async (req, res) => {
     const _id = req.params.id;
     try {
-        const tasssk = await Task.findOne({_id, owner : req.user._id})
         const task = await Task.deleteOne({_id, owner : req.user._id})
 
         if (!task) {
--- src/routers/user.js
+++ src/routers/user.js
@@ -2,6 +2,20 @@ const express = require('express')
 const User = require('../models/user')
 const auth = require('../middleware/auth')
 const router = new express.Router()
+const multer = require('multer')
+const sharp = require('sharp')
+
+const upload = multer({
+  limit : {
+    fileSize : 1000000
+  },
+  fileFilter(req, file, cb){
+    if(!file.originalname.match(/\.(png|jpeg|jpg)$/)){
+      cb(new Error ("Bhosdike"))
+    }
+    cb(undefined, true);
+  }
+})
 
 router.post('/users', async (req, res) => {
     const user = new User(req.body)
@@ -52,6 +66,15 @@ router.get('/users/me', auth, async (req, res) => {
     res.send(req.user)
 })
 
+router.post('/user/me/avatar', auth, upload.single('avatar') , async(req, res)=>{
+    const buffer = await sharp(req.file.buffer).png().resize({width:250, height:250}).toBuffer()
+    req.user.avatar = buffer
+    await req.user.save()
+    res.send("Done Bro!")
+}, (error, req, res, next) =>{
+    res.status(400).send('Madarchood')
+})
+
 router.patch('/users/me', auth, async (req, res) => {
     const updates = Object.keys(req.body)
     const allowedUpdates = ['name', 'email', 'password', 'age']
@@ -70,12 +93,37 @@ router.patch('/users/me', auth, async (req, res) => {
     }
 })
 
+router.get('/users/:id/avatar', async (req, res) => {
+    try {
+        const user = await User.findById(req.params.id)
+        if (!user || !user.avatar){
+          console.log(user)
+          throw new Error("sdds")
+        }
+        
+        res.set('Content-Type','image/jpeg')
+        res.send(user.avatar)
+    } catch (e) {
+        // console.log(e)
+        res.status(404).send()
+    }
+})
+
+router.delete('/users/me/avatar', auth, async (req, res) => {
+    try {
+        req.user.avatar = undefined
+        await req.user.save()
+        res.send()
+    } catch (e) {
+        res.status(500).send()
+    }
+})
+
 router.delete('/users/me', auth, async (req, res) => {
     try {
         await req.user.remove()
         res.send(req.user)
     } catch (e) {
-       console.log(e)
         res.status(500).send()
     }
 })
